e13385fd55eafe7b0a37b87bba8faf8aed8f6e6c
vladpotuk <vladpotuk01@gmail.com>
2024-02-23 19:14:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] array = { 3, 8, 5, 2, 10, 6, 1, 9, 4, 7 };
+
+        Console.WriteLine("Масив:");
+        foreach (var num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        Console.Write("Введіть число для порівняння: ");
+        if (int.TryParse(Console.ReadLine(), out int comparison))
+        {
+            int count = 0;
+            foreach (var num in array)
+            {
+                if (num < comparison)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Кількість значень у масиві менших за {comparison}: {count}");
+        }
+        else
+        {
+            Console.WriteLine("Некоректне введення. Будь ласка, введіть ціле число.");
+        }
+
+        Console.ReadLine(); 
+    }
+}
+
